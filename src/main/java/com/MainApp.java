d5ef27a499850e5a568eb2b24a905a3559bca23f
arulanbarasan <anbu91943@gmail.com>
2025-03-11 15:52:02 +0530

    Fifth commit

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -1,10 +1,24 @@
 package com;
 
+import com.model.Author;
+import com.model.Book;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Book book = new Book();
+        book.setTitle("Subconscious of Mind");
+        Author author = new Author();
+        author.setName("Joseph Murphy");
+        book.setAuthor(author);
+        System.out.println(book);
+        
+        Book book1 = new Book();
+        book1.setTitle("Psychology of Money");
+        Author author1 = new Author();
+        author1.setName("Morgan Housel"); 
+        book1.setAuthor(author1);
+        System.out.println(book1);
 	}
-
 }
